62d1e0c56db56d0a7deb2df11354615670d22201
Cayo Santos <cayo.santos@rift.local>
2025-01-15 19:49:09 -0300

    Salvando alterações no TarefaController antes de pull

--- RegistroDeHoras.Model/Tarefa.cs
+++ RegistroDeHoras.Model/Tarefa.cs
@@ -1,17 +1,16 @@
-using System;
-
 namespace RegistroDeHoras.Model;
 
 public class Tarefa : BaseModel
 {
-    public DateOnly DataDeInicio { get; set; }
-    public TimeOnly HoraDeInicio { get; set; }
-    public TimeOnly HoraDeTermino { get; set; }
-    public TimeOnly HorasDePausa { get; set; }
+    public DateTime Inicio { get; set; }
+    public DateTime Termino { get; set; }
+    public DateTime Pausa { get; set; }
+    public DateTime Reinicio { get; set; }
     public TimeOnly HorasUtilizadas { get; set; }
+    public TimeOnly HorasDePausa { get; set; }
     public string? NumeroAtividade { get; set; }
     public string? Titulo { get; set; }
     public string? Cliente { get; set; }
     public string? Descricao { get; set; }
     public string? StatusDaTarefa { get; set; }
-    }
+}
