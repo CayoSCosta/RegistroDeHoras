326c5425cb6ece4b4e120ad30fd672a59f7613f2
Cayo Costa <cayosantos@gmail.com>
2025-01-24 13:21:18 -0300

    solution de banco de dados

---
+++ RegistroDeHoras/RegistroDeHoras/Tables/Tarefas.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[Tarefas](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Inicio] [datetime2](7) NOT NULL,
+	[Termino] [datetime2](7) NOT NULL,
+	[Pausa] [datetime2](7) NOT NULL,
+	[Reinicio] [datetime2](7) NOT NULL,
+	[HorasUtilizadas] [time](7) NOT NULL,
+	[HorasDePausa] [time](7) NOT NULL,
+	[NumeroTarefa] [nvarchar](max) NULL,
+	[Titulo] [nvarchar](max) NULL,
+	[Cliente] [nvarchar](max) NULL,
+	[Descricao] [nvarchar](max) NULL,
+	[StatusDaTarefa] [nvarchar](max) NULL,
+	[Ativo] [bit] NOT NULL,
+	[DataDeCriacao] [datetime2](7) NOT NULL,
+	[DataDeModificacao] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_Tarefas] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
