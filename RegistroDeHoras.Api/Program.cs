162993f93b61b271bef3537fd2852b885fbb11ad
Cayo Costa <cayosantos@gmail.com>
2025-02-03 19:12:19 -0300

    modal

--- RegistroDeHoras.Api/Program.cs
+++ RegistroDeHoras.Api/Program.cs
@@ -40,7 +40,8 @@ public class Program
         {
             options.AddPolicy("RegistroDeHoras.Client", policy =>
             {
-                policy.WithOrigins("https://localhost:7224") // URL do seu Blazor WebAssembly
+                // policy.WithOrigins("https://localhost:7224") 
+                policy.WithOrigins("http://localhost:5200") 
                       .AllowAnyMethod()
                       .AllowAnyHeader();
             });
